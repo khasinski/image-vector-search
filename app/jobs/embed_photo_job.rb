38436bd8bbc7e683fb9fa565695630484e2a4f01
Krzysztof Hasiński <krzysztof.hasinski@gmail.com>
2025-02-26 21:07:14 +0100

    Step 6,7,8,9: vector search

--- app/jobs/embed_photo_job.rb
+++ app/jobs/embed_photo_job.rb
@@ -2,8 +2,7 @@ class EmbedPhotoJob < ApplicationJob
   queue_as :default
 
   def perform(photo)
-    photo_path = ActiveStorage::Blob.service.path_for(photo.file.key)
-    embedding = $image_embedding.call(photo_path)
+    embedding = $image_embedding.call(photo.file_path)
     photo.update(embedding: embedding)
   end
 end
